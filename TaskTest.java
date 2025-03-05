68b02bd7b3626ee07481cf9b661bbcf96ae6c1a2
Abdul Basith Mohammed <ab430929@dal.ca>
2025-03-05 15:33:13 -0400

    test: add another test for failing case. all test cases pass.

--- TaskTest.java
+++ TaskTest.java
@@ -3,10 +3,17 @@ import org.junit.Test;
 
 public class TaskTest {
     @Test
-    public void testAssignTo() {
+    public void testAssignToPass() {
         Task testtask = new Task();
         Member testMember = new Member(1,"Abdul","CEO");
         Assert.assertTrue(testtask.assignTo(testMember,testtask));
+    }
+    @Test
+    public void testAssignToFail() {
+        Task testtask = new Task();
+        Member testMember = new Member(1,"Abdul","CEO");
+        testtask.assignTo(testMember,testtask);
+        Assert.assertFalse(testtask.assignTo(testMember,testtask));
     }
     @Test
     public void testUpdateStatus(){
