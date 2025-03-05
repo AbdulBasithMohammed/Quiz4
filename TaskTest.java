2dece94c5549ff87730ca60482a10f650f39960a
Abdul Basith Mohammed <ab430929@dal.ca>
2025-03-05 15:13:55 -0400

    feat: create first test case for Task class.

---
+++ TaskTest.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TaskTest {
+    @Test
+    public void testAssignTo() {
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void testUpdateStatus(){
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void testIsOverdue(){
+        Assert.assertFalse(false);
+    }
+}
