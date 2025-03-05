175f0dfdfc2679c43fe5bb4724eb4a54874eedfd
Abdul Basith Mohammed <ab430929@dal.ca>
2025-03-05 15:04:59 -0400

    doc: create initial dummy class with empty methods.

---
+++ Task.java
@@ -0,0 +1,15 @@
+public class Task {
+
+    // Method to assign a task to a particular team member
+    private boolean assignTo(int team_member){
+        return true;
+    }
+    // Method to update task status
+    private boolean updateStatus(String new_status){
+        return true;
+    }
+    // Method to check whether a task is overdue
+    private boolean isOverdue(){
+        return false;
+    }
+}
